2865fbd2fe2ee1eb42b3ecef4f5d091b8b532e1d
Dustin J. Mitchell <dustin@mozilla.com>
2013-09-30 11:27:41 -0400

    actually run p2b

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
+import os
 import flask
+import subprocess
+
+p2b_path = os.path.join(os.path.dirname(__file__), 'p2b')
+p2b_path = './p2b'
  
 application = flask.Flask(__name__)
 
@@ -7,8 +12,15 @@ application = flask.Flask(__name__)
 application.debug=True
  
 @application.route('/')
-def hello_world():
+def ui_redir():
     return flask.redirect(flask.url_for('static', filename='webproto/index.html'))
+
+@application.route('/cgi-bin/webcompiler')
+def webcompiler(program=None):
+    program = flask.request.args.get('program')
+    assert program is not None
+    output = subprocess.Popen([p2b_path, '--instructions', program], stdout=subprocess.PIPE).communicate()[0]
+    return flask.Response(output, mimetype="application/json")
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
